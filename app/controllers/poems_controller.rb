c0d48f801e3178936e3de44df0b28ac446b7df3d
Akira Kubotera <akira.kubotera+github@gmail.com>
2017-05-13 08:03:41 +0000

    Deve13 デバック完了！

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,5 +1,5 @@
 class PoemsController < ApplicationController
-  before_action :set_poem, only: [:show, :edit, :destroy]
+  before_action :set_poem, only: [:show, :edit, :destroy,:update]
 
   # GET /poems
   # GET /poems.json
@@ -25,7 +25,6 @@ class PoemsController < ApplicationController
   # POST /poems.json
   def create
     @poem = Poem.new(poem_params)
-
     respond_to do |format|
       if @poem.save
         format.html { redirect_to @poem, notice: 'Poem was successfully created.' }
@@ -40,6 +39,7 @@ class PoemsController < ApplicationController
   # PATCH/PUT /poems/1
   # PATCH/PUT /poems/1.json
   def update
+    @poem.update(poem_params)
     respond_to do |format|
       if @poem.update(poem_params)
         format.html { redirect_to @poem, notice: 'Poem was successfully updated.' }
@@ -69,6 +69,6 @@ class PoemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def poem_params
-      params.require(:poem).permit(:content, :auther)
+      params.require(:poem).permit(:title,:content, :auther)
     end
 end
